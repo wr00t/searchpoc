e9b29809ac9f5de7b963b67a2f96d574e6542bb3
Valerio Casalino <casalinovalerio.cv@gmail.com>
2020-12-17 13:45:21 +0100

    Functioning, better arg parsing need to be implemented

--- searchpoc.py
+++ searchpoc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 """
+=======THIS IS OVERLY DOCUMENTED=======
 DESCRIPTION:
 Tool to search on the internet (shodan, youtube, cvebase, github) for 
 public PoCs, given one or more CVEs in the format of CVE-XXXX-XXXX.
@@ -23,6 +24,7 @@ import json
 import re
 import os
 import urllib.request
+import sys
 
 # External
 from youtubesearchpython import SearchVideos
@@ -41,16 +43,22 @@ def message(msg):
 
 # Print all results in a list with custom header
 def print_results(header, res_lst):
+    if res_lst == 0:
+        return
     print("[+][+]", header)
     for res in res_lst:
         print(res)
 
 # Search videos regarding the asked cve using some Google style keywords
 def search_youtube(cve):
-    to_return = []
+    
     # https://pypi.org/project/youtube-search-python/
+    # Search results and load them as json objects
     search = SearchVideos(f'intitle:"{cve}" + "poc"', offset = 1, mode = "json", max_results = 3)
     jresults = json.loads(search.result())
+    
+    # Extract the links and return them
+    to_return = []
     for res in jresults["search_result"]:
         link = res["link"]
         to_return.append(link)
@@ -59,27 +67,50 @@ def search_youtube(cve):
 # Notation of raw page on github is: 
 # https://<url>/cvebase/cvebase.com/main/cve/2000/1xxx/CVE-2000-1209.md
 # So from cve (CVE-1234-5678) we have to take 5xxx
-def search_cvebase(cve):    
-    folder = f"{cve[9]}xxx"
+def search_cvebase(cve):
+    
+    # Check for longer numbers (eg: CVE-2019-11111)
+    if len(cve) == 13:    
+        folder = f"{cve[9]}xxx"
+    elif len(cve) == 14:
+        folder = f"{cve[9:10]}xxx"
+    
+    # Getting the year from the first part
     year = f"{cve[4:8]}"
-    url = CVEBASE_URL.format(year, folder, cve)
+    
+    # https://raw.githubusercontent.com/cvebase/cvebase.com/main/cve/{}/{}/{}.md
+    url = CVEBASE_URL.format(year, folder, cve) 
+    
+    # Connect, or skip if not HTTP 200
     try:
         response = urllib.request.urlopen(url)
     except urllib.error.HTTPError:
         return []
+    
+    # Find all urls in response returning them as list of str
     html = response.read().decode("utf-8")
     return re.findall(r'(https?://[^\s]+)', html)
 
+
 # Using GH APIs https://docs.github.com/en/free-pro-team@latest/rest/reference/search
 def search_github(cve):
+    
+    # https://api.github.com/search/repositories?q={}&page=1
     url = GITHUB_API_Q.format(cve)
+    
+    # Connect, or skip if not HTTP 200
     try:
         response = urllib.request.urlopen(url)
     except urllib.error.HTTPError:
         return []
+    
+    # Refer to the api to know the json format
     ghresp = json.loads(response.read().decode("utf-8"))
-    if ghresp["total_count"] == 0:
-        return []
+
+    # Return if no result is shown
+    if ghresp["total_count"] == 0: return []
+    
+    # From json extract links to repos
     to_return = []
     for item in ghresp["items"]:
         to_return.append(item["html_url"])
@@ -89,19 +120,14 @@ def search_github(cve):
 
 def main():
 
-    # Get youtube results
-    yt = search_youtube("test")
-    if len(yt) != 0:
-        print_results("FROM YOUTUBE (https://www.youtube.com/)", yt)
+    if len(sys.argv) != 2:
+        message("Pass just 1 cve as argument")
+        exit(1)
 
-    cb = search_cvebase("CVE-2000-1209")
-    if len(cb) != 0:
-        print_results("FROM CVEBASE (https://www.cvebase.com/)", cb)
-
-    gh = search_github("CVE-2000-1350")
-    if len(gh) != 0:
-        print_results("FROM GITHUB (https://github.com/)", gh)
-
+    print(f"==={sys.argv[1]}===")
+    print_results("FROM YOUTUBE (https://www.youtube.com/)", search_youtube(sys.argv[1]))
+    print_results("FROM CVEBASE (https://www.cvebase.com/)", search_cvebase(sys.argv[1]))
+    print_results("FROM GITHUB (https://github.com/)", search_github(sys.argv[1]))
     return
 
 #######################################################################
